885af758f0b04452601a7d0ca6cb38bc09a21666
nagasako-tetsuya <t.nagasako@adseed.co.jp>
2023-12-02 00:08:04 +0900

    questionsの取得API作成

---
+++ src/handlers/get-all-questions-by-id.ts
@@ -0,0 +1,60 @@
+const tableName = process.env.QUESTIONS_TABLE;
+
+const dynamodb = require("aws-sdk/clients/dynamodb");
+const docClient = new dynamodb.DocumentClient(
+  process.env.AWS_SAM_LOCAL
+    ? {
+        credentials: {
+          accessKeyId: "DUMMYACCESSKEY",
+          secretAccessKey: "DUMMYSECRETACCESSKEY",
+        },
+        region: "ap-northeast-1",
+        endpoint: "http://dynamodb-local:8000",
+      }
+    : {}
+);
+
+exports.getAllQuestionsByIdHandler = async (event) => {
+  const user_id = Number(event.pathParameters.id);
+  let response: { statusCode: number; body: any } = {
+    statusCode: 400,
+    body: {},
+  };
+
+  try {
+    const params = {
+      TableName: tableName,
+      FilterExpression: "user_id = :user_idValue",
+      ExpressionAttributeValues: {
+        ":user_idValue": user_id,
+      },
+    };
+    const data = await docClient.scan(params).promise();
+    const items = data.Items;
+
+    response = {
+      statusCode: 200,
+      body: JSON.stringify(items),
+    };
+  } catch (error) {
+    console.error("エラー:", error);
+
+    if (error.code === "ResourceNotFoundException") {
+      response = {
+        statusCode: 404,
+        body: "DynamoDBテーブルが見つかりません。",
+      };
+    } else {
+      response = {
+        statusCode: 500,
+        body: "内部サーバーエラー",
+      };
+    }
+  }
+
+  // 全てのログは CloudWatch に書き込まれます
+  console.info(
+    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+  );
+  return response;
+};
