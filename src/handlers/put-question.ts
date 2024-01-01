e49bdcb54c283038905dae8a7161da6c8f43e97d
fumiyama6256 <au.ver.c.e.2@gmail.com>
2024-01-01 23:18:26 +0900

    js->tsファイルに変更

--- src/handlers/put-question.ts
+++ src/handlers/put-question.ts
@@ -1,4 +1,5 @@
-// Create clients and set shared const values outside of the handler.
+// Get the DynamoDB table name from environment variables
+const tableName = process.env.QUESTIONS_TABLE;
 
 // Create a DocumentClient that represents the query to add an item
 const dynamodb = require('aws-sdk/clients/dynamodb');
@@ -15,13 +16,29 @@ const docClient = new dynamodb.DocumentClient(
     : {}
 );
 
-// Get the DynamoDB table name from environment variables
-const tableName = process.env.QUESTIONS_TABLE;
+// TODO 他箇所でも使用するのであれば共通化する
+interface Event {
+    httpMethod: string;
+    body: string;
+    path: string;
+}
+
+interface Body {
+    category: string;
+    title: string;
+    question_text: string;
+}
+
+interface Response {
+    statusCode: number;
+    body: string;
+}
 
 /**
  * A simple example includes a HTTP post method to add one item to a DynamoDB table.
  */
-exports.putQuestionHandler = async (event) => {
+exports.putQuestionHandler = async (event: Event) => {
+    console.log('イベント：', event);
     if (event.httpMethod !== 'POST') {
         throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
     }
@@ -29,33 +46,29 @@ exports.putQuestionHandler = async (event) => {
     console.info('received:', event);
 
     // Get id and name from the body of the request
-    const body = JSON.parse(event.body);
+    const body: Body = JSON.parse(event.body);
     // TODO ログ確認
     console.log('ログ出力で確認：', body);
-    const id = body.id;
-    const name = body.name;
+    const category = body.category;
+    const title = body.title;
+    const question_text = body.question_text;
 
     // Creates a new item, or replaces an old item with a new item
     // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
-    let response = {};
+    let response: Response = { statusCode: 0, body: '' };
 
     try {
         const params = {
             TableName : tableName,
-            Item: { id : id, name: name }
+            Item: { category : category, title: title, question_text: question_text}
         };
 
-        const result = await docClient.put(params).promise();
+        const data = await docClient.put(params).promise();
 
         response = {
             statusCode: 200,
-          body: JSON.stringify(body),
-          headers: {
-            'Access-Control-Allow-Origin': 'http://localhost:3001', // http://localhost:3001 からのアクセスだけを許可
-            // 必要に応じて他のCORSヘッダーを追加
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            body: JSON.stringify(data),
         }
-        };
     } catch (ResourceNotFoundException) {
         response = {
             statusCode: 404,
@@ -65,5 +78,8 @@ exports.putQuestionHandler = async (event) => {
 
     // All log statements are written to CloudWatch
     console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+
     return response;
 };
+
+export {};
